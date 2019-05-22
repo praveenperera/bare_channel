b3a0db61978228a16cc4eb6b522fc6578b0d2606
Praveen Perera <ppraveen25@gmail.com>
2019-05-22 19:34:27 -0400

    Set logger level to warn

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :bare_channel, BareChannelWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :warn
 
 # ## SSL Support
 #
